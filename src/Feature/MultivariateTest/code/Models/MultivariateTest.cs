3c3c19bf18b5de0bca3503661f0122f03fb510d1
Kevin Schuurmans <fruitwasp@users.noreply.github.com>
2018-03-06 11:12:27 +0100

    habitat tests

---
+++ src/Feature/MultivariateTest/code/Models/MultivariateTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.MultivariateTest.Models
+{
+    public class MultivariateTest
+    {
+        public string Title { get; set; }
+    }
+}
