fff7ce7facc559a54817ad14ead125e5dd09afbc
Kevin Schuurmans <fruitwasp@users.noreply.github.com>
2018-03-07 11:35:46 +0100

    initial test item fetch

---
+++ src/Feature/MultivariateTest/code/Repositories/IMultivariateTestRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Feature.MultivariateTest.Repositories
+{
+    public interface IMultivariateTestRepository
+    {
+        IEnumerable<Item> Get(Item contextItem);
+        IEnumerable<Item> GetLatest(Item contextItem, int count);
+    }
+}
---
+++ src/Feature/MultivariateTest/code/Templates.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.Feature.MultivariateTest
+{
+    using Sitecore.Data;
+
+    public struct Templates
+    {
+        public struct MultivariateTest
+        {
+            public static readonly ID ID = new ID("{2F75C8AF-35FC-4A88-B585-7595203F442C}");
+
+            public struct Fields
+            {
+                public static readonly ID Title = new ID("{BD9ECD4A-C0B0-4233-A3CD-D995519AC87B}");
+                public const string Title_FieldName = "MultivariateTestTitle";
+            }
+        }
+
+        public struct MultivariateTestFolder
+        {
+            public static readonly ID ID = new ID("{2F75C8AF-35FC-4A88-B585-7595203F442C}");
+        }
+    }
+}
