685d8108b09e97d84c05ba811e56c9508e1d984e
Kevin Schuurmans <fruitwasp@users.noreply.github.com>
2018-03-08 16:54:35 +0100

    progress on getting test variants

--- src/Feature/MultivariateTest/code/Controllers/MultivariateTestController.cs
+++ src/Feature/MultivariateTest/code/Controllers/MultivariateTestController.cs
@@ -4,15 +4,37 @@ namespace Sitecore.Feature.MultivariateTest.Controllers
 {
     using Sitecore.ContentTesting;
     using Sitecore.ContentTesting.ContentSearch.Models;
+    using Sitecore.ContentTesting.Model;
+    using Sitecore.ContentTesting.Model.Data.Items;
+    using Sitecore.Data;
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class MultivariateTestController : Controller
     {
         public ActionResult Index()
         {
             IEnumerable<TestingSearchResultItem> testingSearchResultItems = ContentTestingFactory.Instance.ContentTestStore.GetActiveTests();
-          
-            return View(testingSearchResultItems);
+
+            var testingSearchResultItem = testingSearchResultItems.First();
+
+            var item = Database.GetItem(testingSearchResultItem.Uri);
+            var testDefinitionItem = TestDefinitionItem.Create(item);
+
+            var hostItem = item.Database.GetItem(testingSearchResultItem.HostItemUri);
+
+            ITestConfiguration configuration = ContentTestingFactory.Instance.ContentTestStore.LoadTestForItem(hostItem, testDefinitionItem);
+
+            IEnumerable<TestExperience> experiences = configuration.Experiences;
+
+            var textExperience = experiences.First();
+
+            IEnumerable<Guid> variantIds = textExperience.Variants;
+
+            var variantId = variantIds.First().ToString();
+
+            return View(null, null, variantId);
         }
     }
 }
