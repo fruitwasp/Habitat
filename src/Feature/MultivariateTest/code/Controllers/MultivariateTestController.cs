57627cbcaf914d705cd37743489901eae39ca60f
Kevin Schuurmans <fruitwasp@users.noreply.github.com>
2018-03-09 14:10:05 +0100

    uitpluizen

--- src/Feature/MultivariateTest/code/Controllers/MultivariateTestController.cs
+++ src/Feature/MultivariateTest/code/Controllers/MultivariateTestController.cs
@@ -6,7 +6,9 @@ namespace Sitecore.Feature.MultivariateTest.Controllers
     using Sitecore.ContentTesting.ContentSearch.Models;
     using Sitecore.ContentTesting.Model;
     using Sitecore.ContentTesting.Model.Data.Items;
+    using Sitecore.ContentTesting.Models;
     using Sitecore.Data;
+    using Sitecore.Data.Items;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -22,19 +24,54 @@ namespace Sitecore.Feature.MultivariateTest.Controllers
             var item = Database.GetItem(testingSearchResultItem.Uri);
             var testDefinitionItem = TestDefinitionItem.Create(item);
 
+            //return View(null, null, testDefinitionItem);
+
             var hostItem = item.Database.GetItem(testingSearchResultItem.HostItemUri);
 
             ITestConfiguration configuration = ContentTestingFactory.Instance.ContentTestStore.LoadTestForItem(hostItem, testDefinitionItem);
 
-            IEnumerable<TestExperience> experiences = configuration.Experiences;
+            return View(null, null, configuration.DeviceName);
 
-            var textExperience = experiences.First();
+            //IEnumerable<TestExperience> experiences = configuration.Experiences;
 
-            IEnumerable<Guid> variantIds = textExperience.Variants;
+            //TestExperience textExperience = experiences.First();
 
-            var variantId = variantIds.First().ToString();
+            //IEnumerable<Guid> variantIds = textExperience.Variants;
 
-            return View(null, null, variantId);
+            //var variantId = variantIds.First();
+
+            //return View(null, null, variantId);
+
+            //var variantItem = Context.Database.GetItem(new ID(variantId));
+            //return View(null, null, variantItem);
+
+            //var variables = configuration.Variables;
+
+            //TestSet testSet = configuration.TestSet;
+
+            //TestValueCollection testValueCollection = testSet.Variables[0].Values;
+            //var testValue = testValueCollection[0];
+
+            //return View(null, null, testValue);
         }
+
+        private TestDebugInfo CreateTestDebugInfo(Item item)
+        {
+            TestDebugInfo testDebugInfo = new TestDebugInfo
+            {
+                itemPath = "",
+                itemUri = item.Uri,
+                templateName = item.TemplateName
+            };
+
+            return testDebugInfo;
+        }
+    }
+
+    public struct TestDebugInfo
+    {
+        public string itemPath;
+        public ItemUri itemUri;
+        public string templateName;
     }
 }
